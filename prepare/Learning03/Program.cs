06073a7d1e45975cd596a71658f18fdad42f2ad7
willpatbarr <wpcbarr20@gmail.com>
2024-05-11 23:28:50 -0600

    completed learning activity 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    {// create two new instances of job
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        job1.Display();
+        job2.Display();
+
+        // create new instance of resume
+        Resume myResume = new Resume("John Doe");
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        myResume.Display();
     }
 }
